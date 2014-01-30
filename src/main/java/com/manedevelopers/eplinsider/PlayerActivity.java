d8b4e11b5db6caa89911459fe487f0939b947797
Mikael Mengistu <mikaelm@mit.edu>
2014-01-29 21:38:35 -0500

    Created the player list item and PlayerAdapter to fix the spacing on squadlist activity

--- src/main/java/com/manedevelopers/eplinsider/PlayerActivity.java
+++ src/main/java/com/manedevelopers/eplinsider/PlayerActivity.java
@@ -6,6 +6,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
@@ -29,6 +31,7 @@ public class PlayerActivity extends Activity {
         playerName = intent.getStringExtra("Player");
 
         tvplayer_name = (TextView)findViewById(R.id.tvPlayerName);
+        new FetchItemsTask().execute();
 
         tvplayer_name.setText(playerName);
 
@@ -139,10 +142,7 @@ public class PlayerActivity extends Activity {
 
 
         @Override
-        protected void onPostExecute(ArrayList<Player> Players){
-            tvplayer_name.setText("Hi");
-
-
+        protected void onPostExecute(String[] playerStats){
 
 
         }
